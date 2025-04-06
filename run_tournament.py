f2c9c937a1fddf855ba79467ed50708cf9d27eed
Georgi Dimitrov <g.h.dimitrov@student.tudelft.nl>
2025-04-06 16:45:57 +0200

    Run tournament settings

--- run_tournament.py
+++ run_tournament.py
@@ -39,81 +39,22 @@ tournament_settings = {
         {
             "class": "agents.stupid_agent.stupid_agent.StupidAgent",
         },
-        {
-            "class": "agents.CSE3210.agent2.agent2.Agent2",
-        },
-        {
-            "class": "agents.CSE3210.agent3.agent3.Agent3",
-        },
-        {
-            "class": "agents.CSE3210.agent7.agent7.Agent7",
-        },
-        {
-            "class": "agents.CSE3210.agent11.agent11.Agent11",
-        },
-        {
-            "class": "agents.CSE3210.agent14.agent14.Agent14",
-        },
-        {
-            "class": "agents.CSE3210.agent18.agent18.Agent18",
-        },
-        {
-            "class": "agents.CSE3210.agent19.agent19.Agent19",
-        },
-        {
-            "class": "agents.CSE3210.agent22.agent22.Agent22",
-        },
-        {
-            "class": "agents.CSE3210.agent24.agent24.Agent24",
-        },
-        {
-            "class": "agents.CSE3210.agent25.agent25.Agent25",
-        },
-        {
-            "class": "agents.CSE3210.agent26.agent26.Agent26",
-        },
-        {
-            "class": "agents.CSE3210.agent27.agent27.Agent27",
-        },
-        {
-            "class": "agents.CSE3210.agent29.agent29.Agent29",
-        },
-        {
-            "class": "agents.CSE3210.agent32.agent32.Agent32",
-        },
-        {
-            "class": "agents.CSE3210.agent33.agent33.Agent33",
-        },
-        {
-            "class": "agents.CSE3210.agent41.agent41.Agent41",
-        },
-        {
-            "class": "agents.CSE3210.agent43.agent43.Agent43",
-        },
-        {
-            "class": "agents.CSE3210.agent50.agent50.Agent50",
-        },
-        {
-            "class": "agents.CSE3210.agent52.agent52.Agent52",
-        },
-        {
-            "class": "agents.CSE3210.agent55.agent55.Agent55",
-        },
-        {
-            "class": "agents.CSE3210.agent58.agent58.Agent58",
-        },
-        {
-            "class": "agents.CSE3210.agent61.agent61.Agent61",
-        },
-        {
-            "class": "agents.CSE3210.agent64.agent64.Agent64",
-        },
-        {
-            "class": "agents.CSE3210.agent67.agent67.Agent67",
-        },
-        {
-            "class": "agents.CSE3210.agent68.agent68.Agent68",
-        },
+        # {
+        #     "class": "agents.group21_adaptive_learner_agent.group21_adaptive_learner_agent.Group21AdaptiveLearnerAgent",
+        #     "parameters": {"storage_dir": "agent_storage/Group21Agent",},
+        # },
+        # {
+        #     "class": "agents.group21_strategic_conceder_agent.group21_strategic_conceder_agent.Group21StrategicConcederAgent",
+        #     "parameters": {"storage_dir": "agent_storage/TemplateAgent"},
+        # },
+        # {
+        #     "class": "agents.group21_deadline_pusher_agent.group21_deadline_pusher_agent.DeadlinePusher",
+        #     "parameters": {"storage_dir": "agent_storage/DeadlinePusher"},
+        # },
+        # {
+        #     "class": "agents.group21_clueless_agent.group21_clueless_agent.CluelessAgent",
+        #     "parameters": {"storage_dir": "agent_storage/TemplateAgent"},
+        # },
     ],
     "profile_sets": [
         ["domains/domain00/profileA.json", "domains/domain00/profileB.json"],
